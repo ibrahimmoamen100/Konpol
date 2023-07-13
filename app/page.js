5d09d160c67d263eb2ea1af7a66f8fa27a37a019
ibrahimmoamen100 <ibrahim.moamen100@gmail.com>
2023-07-13 06:58:17 +0300

    new update

--- app/page.js
+++ app/page.js
@@ -1,20 +1,27 @@
 
 import BottomNavbar from "@/components/BottomNavbar";
+import Card from "@/components/Card";
 import CounterPage from "@/components/CounterPage";
 import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
-
+// import ProductCard from "@/components/ProductCard";
+// import Slider from "@/components/Slider";
+import Stats from "@/components/Stats";
 import Topnavbar from "@/components/Topnavbar";
-export default function page() {
+export default function Home() {
   return (
     <div>
       <Topnavbar />
       <Navbar /> 
+      
       <BottomNavbar/>
       <CounterPage/>
+      {/* <Slider /> */}
       <div className="bg-zinc-100">
 
       </div>
+      {/* <Card/> */}
+      <Stats/>
       <Footer /> 
     </div>
   );
